527ca858f06cd81e28034a68fbef5578714c9d71
白森森 <799887603@qq.com>
2020-09-08 20:15:20 +0800

    第四次修改

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,46 @@
  * @Author: 白金鑫
  * @Date: 2020-09-08 17:51:04
  * @LastEditors: 白金鑫
- * @LastEditTime: 2020-09-08 19:35:53
+ * @LastEditTime: 2020-09-08 20:14:26
 -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     vue
+    <comped :arr='arr' v-if="flag" @isdefale="flag=false" :title.sync='title' >
+        <h1>{{title}}</h1>
+    </comped>
+    <button @click="flag=true">
+        点击
+    </button>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import comped from './components/comped'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+      comped
+  },
+  data(){
+      return {
+          flag:false,
+          arr:['开心','不开心'],
+          title:'你开心吗？'
+      }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1{
+    font-size: 36px;
+}
+*{
+    
+    list-style: none;
 }
 </style>
---
+++ src/components/comped.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 白金鑫
+ * @Date: 2020-09-08 19:42:50
+ * @LastEditors: 白金鑫
+ * @LastEditTime: 2020-09-08 20:13:19
+-->
+<template>
+  <div @click='$emit("isdefale")' class="alert">
+      <slot></slot>
+      <ul>
+          <li v-for="(item,index) in arr" :key="index">
+
+            {{item}}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        arr:Array
+    }
+}
+</script>
+
+<style>
+.alert{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    left: 0;
+    top: 0;
+    background: rgba(0, 0, 0, 0.5);
+}
+
+ul{
+    width: 200px;
+    height: 100px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background: #fff;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    z-index: 5;
+    transform: translate(-50%,-50%);
+}
+</style>
