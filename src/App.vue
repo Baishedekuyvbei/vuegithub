96a332c446e2b1018dd9206043a8869649354bed
白森森 <799887603@qq.com>
2020-09-08 19:37:52 +0800

    第二次修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 白金鑫
+ * @Date: 2020-09-08 17:51:04
+ * @LastEditors: 白金鑫
+ * @LastEditTime: 2020-09-08 19:35:53
+-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    vue
   </div>
 </template>
 
